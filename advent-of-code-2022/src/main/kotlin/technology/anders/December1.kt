b9deb5adb6307ffc6001a0ef146f503afab3783c
Anders Ullnæss <andersullnass@gmail.com>
2022-12-02 19:37:23 +0100

    Implement December 1 solution

---
+++ advent-of-code-2022/src/main/kotlin/technology/anders/December1.kt
@@ -0,0 +1,30 @@
+package technology.anders
+
+import java.io.File
+
+fun main() {
+    ElfCalorieCounter().findMaxCalories()
+}
+
+class ElfCalorieCounter {
+    fun findMaxCalories() {
+        val caloriesPerElfList = mutableListOf<Int>()
+
+        var currentElfCalories = 0
+
+        File("src/main/resources/december_1_input.txt").readLines().forEach {
+            if (it.isNotBlank()) {
+                currentElfCalories += it.toInt()
+            } else {
+                caloriesPerElfList.add(currentElfCalories)
+                currentElfCalories = 0
+            }
+        }
+
+        println("First answer: ${caloriesPerElfList.max()}")
+
+        println("Second answer: ${caloriesPerElfList.sortedDescending().take(3).sum()}")
+    }
+}
+
+
